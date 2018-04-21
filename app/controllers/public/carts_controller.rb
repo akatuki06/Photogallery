d4034100dab00a6f39c837f9cdb312a56bde2db3
akatuki06 <akatuki0623@gmail.com>
2018-04-21 18:30:19 +0900

    cart_1.22

--- app/controllers/public/carts_controller.rb
+++ app/controllers/public/carts_controller.rb
@@ -4,6 +4,13 @@ class Public::CartsController < Public::Base
   def show
   end
 
+  def destroy
+    @cart = current_cart
+    @cart.destroy
+    session[:cart_id] = nil
+    redirect_to root_path, notice: 'カートが空になりました。'
+  end
+
   private
     def set_cart
       @cart = Cart.find(params[:id])
