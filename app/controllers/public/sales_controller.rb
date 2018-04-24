904b76531015ed3bfa7dbd5edfa80926e1d7260e
akatuki06 <akatuki0623@gmail.com>
2018-04-24 23:09:37 +0900

    sale完

--- app/controllers/public/sales_controller.rb
+++ app/controllers/public/sales_controller.rb
@@ -1,8 +1,7 @@
 class Public::SalesController < Public::Base
 
 	before_action :authenticate_user!, except: [:index, :show]
-	before_action :artist_user, except: [:index, :show]
-  	before_action :ensure_correct_artist, only: [:edit, :update, :soft_delete]
+	before_action :artist_user
 
   def index
   	id = current_user.artist.id
@@ -14,13 +13,6 @@ class Public::SalesController < Public::Base
 
   def artist_user
   	redirect_to root_path unless current_user.is_artist?
-  end
-
-  def ensure_correct_artist
-     @work = Work.find(params[:id])
-    if @work.artist.user != current_user || @work.is_deleted == true
-       redirect_to root_path, notice: "権限がありません"
-      end
   end
 
 end
