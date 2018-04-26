cefd27c59642faa75c71d06a473330773d110764
akatuki06 <akatuki0623@gmail.com>
2018-04-26 14:59:25 +0900

    いいね遠藤さんヘルプ後

--- app/controllers/public/clip_artists_controller.rb
+++ app/controllers/public/clip_artists_controller.rb
@@ -1,21 +1,26 @@
 class Public::ClipArtistsController < Public::Base
 
 	def create
-		artist = Artist.find(params[:artist_id])
+
+		# @like = Clip_artist.create(user_id: current_user.id, artist_id: params[:artist_id])
+		# @artist = Artist.find(params[:id])
+		@artist = Artist.find(params[:artist_id])
 		if user_signed_in?
-		clip = current_user.clip_artists.new(artist_id: artist.id)
+		clip = current_user.clip_artists.new(artist_id: @artist.id)
 		clip.save
-		redirect_to artist_path(artist)
-		return
+		# redirect_to artist_path(@artist)
+		# return
 		end
-		redirect_to artist_path(artist), notice: "会員登録しよう！"
+		# redirect_to artist_path(artist), notice: "会員登録しよう！"
 	end
 
 	def destroy
-		artist = Artist.find(params[:artist_id])
+		# like = Clip_artist.find_by(user_id: current_user.id, artist_id: params[:artist_id])
+		# like.destroy
+		@artist = Artist.find(params[:artist_id])
 		clip = current_user.clip_artists.find_by(artist_id: params[:artist_id])
 		clip.destroy
-		redirect_to artist_path(artist)
+		# redirect_to artist_path(artist)
 	end
 
 end
